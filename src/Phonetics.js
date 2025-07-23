d789e0cdb80bad3750894e04c423ff53c2e7db7e
SiiyaN <87692603+SiiyaN@users.noreply.github.com>
2025-07-23 22:43:57 +0200

    Update Phonetics.js
    Simplified the phonetics component to get it to display on the application.

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetc.audio} target="_blank" rel="noopener noreferrer">
-        Listen
-      </a>
-      <br />
-      {props.phonetc.text}
-    </div>
-  );
+  if (props.phonetic) {
+    return (
+      <div className="Phonetics">
+        <p>{props.phonetic}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
