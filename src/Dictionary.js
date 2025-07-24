12e28d2b6f5ba89ccb024778de6ff99a93351ed3
SiiyaN <87692603+SiiyaN@users.noreply.github.com>
2025-07-24 22:57:54 +0200

    Added a photos component and styling to display images related to the searched word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,11 +4,13 @@ import Definitions from "./Definitions";
 import "./Dictionary.css";
 import Synonyms from "./Synonyms";
 import Phonetics from "./Phonetics";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [definitions, setDefinitions] = useState(null);
   let [phonetic, setPhonetic] = useState("");
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -16,11 +18,20 @@ export default function Dictionary() {
     setPhonetic(response.data.phonetic);
   }
 
+  function handleImageResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+  }
+
   function search(event) {
     event.preventDefault();
     let apiKey = "f063aad8tb9d2a804775off7e6bf14bb";
     let apiRrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiRrl).then(handleResponse);
+
+    let imgApiKey = "f063aad8tb9d2a804775off7e6bf14bb";
+    let imgApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imgApiKey}`;
+    axios.get(imgApiUrl).then(handleImageResponse);
   }
 
   function handleKeywordChange(event) {
@@ -36,6 +47,7 @@ export default function Dictionary() {
       <Definitions definitions={definitions} word={keyword} />
       <Phonetics phonetic={phonetic} />
       <Synonyms definitions={definitions} />
+      <Photos photos={photos} />
     </div>
   );
 }
